e2ca1ec78bc3981871425c60ab68bce3834009ca
PeshkovDmitry <peshkov-dv@yandex.ru>
2023-03-30 18:39:39 +0300

    Задача про пирамидки

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача про пирамидки
-void Towers(string with = "1", string on = "3", string some = "2", int count = 3)
+void Towers(string with = "1", string on = "3", string some = "2", int count = 2)
 {
     if (count > 1) Towers(with, some, on, count - 1);
     Console.WriteLine($"{with} >> {on}");
